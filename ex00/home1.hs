530d3cd186ec12e505f09841c6b0a1004c581af1
kotto5 <kotto555555@gmail.com>
2024-03-18 20:07:46 +0900

    exercise 2

--- ex00/home1.hs
+++ ex00/home1.hs
@@ -9,3 +9,8 @@ toDigits list = toReverse (toDigitsRev list)
 toReverse :: [Integer] -> [Integer]
 toReverse []      = []
 toReverse (x:xs)  = toReverse xs ++ [x]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:xs)
+ | length xs `mod` 2 == 1 = x * 2 : doubleEveryOther xs
+ | otherwise = x : if length xs > 0 then doubleEveryOther xs else []
