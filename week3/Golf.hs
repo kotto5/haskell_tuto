36a4f753fcb24802aefd1bb1d8050a44e8168a88
kotto5 <kotto555555@gmail.com>
2024-04-20 13:51:42 +0900

    社協なう

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -1,27 +1,22 @@
-module Golf where
-import Data.ByteString (split)
-
-everyElement :: Int -> [a] -> [a]
-everyElement _ [] = []
-everyElement n ls = 
-    let (ls1, ls2) = splitAt n ls
-    in head ls1 : everyElement n ls2
-
-
--- skips :: [a] -> [[a]]
--- [a] -> [[a | filter (a % n) a ]]
-
--- everyElement ls = map head (map (drop (n - 1)) ls)
-
+-- First list is the input itself, second list contains only every second
+-- element of the input list, etc.
+-- > skips "ABCD"        == ["ABCD", "BD", "C", "D"]
+-- > skips "hello!"      == ["hello!", "el!", "l", "o", "!"]
+-- > skips [1]           == [[1]]
+-- > skips [True, False] == [[True,False], [False]]
+-- > skips []            == []
 skips :: [a] -> [[a]]
-skips [] = []
-skips xs = map (\n -> everyNth n xs) [1..length xs]
-    where everyNth n = map snd . filter (\(i, _) -> i `mod` n == 0) . zip [1..]
+-- As mentioned in the specification for exercise 1, the output list is of the
+-- same length as the input list.
+skips lst = [each i lst | i <- [1..length lst]]
 
+-- Returns each @n@-th element of the list.
+-- > each 2 [1, 2, 3, 4] = [2, 4]
+-- > each 2 [] = []
+each :: Int -> [a] -> [a]
+-- Using list comprehension. Index list returns exactly the indices of every
+-- n-th element. Is safe because when the @lst@ is empty then the index list is
+-- also empty. And indices go from 0 to (length lst) - 1, so also we don't
+-- index anything to big.
+each n lst = [lst !! i | i <- [n-1, n-1+n..length lst - 1]]
 
-
-
--- skips l = 
-
--- skip
--- every 1 element, every 2 element...
