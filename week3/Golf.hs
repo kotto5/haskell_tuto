fe5b026696b030445d5fe2afd72b2f867c0b7679
kotto5 <kotto555555@gmail.com>
2024-04-21 17:41:19 +0900

    Golf finished@

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -1,3 +1,7 @@
+import Data.Char (toUpper, chr)
+import Data.List (delete)
+import Text.Printf (IsChar(toChar))
+
 -- First list is the input itself, second list contains only every second
 -- element of the input list, etc.
 -- > skips "ABCD"        == ["ABCD", "BD", "C", "D"]
@@ -20,3 +24,49 @@ each :: Int -> [a] -> [a]
 -- index anything to big.
 each n lst = [lst !! i | i <- [n-1, n-1+n..length lst - 1]]
 
+toUpperFunc s = [toUpper c | c <- s]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:zs)
+  | y > x && y > z = y : localMaxima (y:z:zs)
+  | otherwise      = localMaxima (y:z:zs)
+localMaxima _ = []
+
+histogram :: [Integer] -> String
+histogram lst = doHist lst ++ "==========\n0123456789\n"
+
+doHist :: [Integer] -> String
+doHist [] = ""
+doHist lst = doHist rest ++ line (unique lst)
+    where
+        unique_lst = unique lst
+        rest = diff lst unique_lst
+
+-- [0,1,2] == "***       \n"
+--             0123456789
+line :: [Integer] -> String
+line [] = "" 
+line xs = [toHist i xs | i <- [0..9]] ++ "\n"
+
+-- 1, [1] == '*'
+-- 1, [0] == ' '
+toHist :: Integer -> [Integer] -> Char
+toHist i xs = 
+    if i `elem` xs
+    then '*'
+    else ' '
+
+unique :: [Integer] -> [Integer]
+unique [] = []
+unique (x:xs) = x : unique (filter (/=x) xs)
+
+diff :: [Integer] -> [Integer] -> [Integer]
+diff (x:xs) ys =
+    if x `elem` ys
+    then diff xs (delete x ys)
+    else x : diff xs ys
+diff [] _ = []
+
+-- " * * \n==========\n0123456789\n"
+-- "   * *    \n==========\n0123456789\n"
+-- "0123456789"
