b1ae66d63d80bf880fb50307d26eee7e5bae6925
kotto5 <kotto555555@gmail.com>
2024-04-23 23:58:07 +0900

    y combinator 使ってみた

--- week4/homework.hs
+++ week4/homework.hs
@@ -13,3 +13,18 @@ fun2 1 = 0
 fun2 n
     | even n = n + fun2 (n `div` 2)
     | otherwise = fun2 (3 * n + 1)
+
+y :: (a -> a) -> a
+y x = x (y x)
+
+utilFun2 :: Integral a => (a -> a) -> a -> a
+utilFun2 f n = if even n then n `div` 2 else f (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' = sum . takeWhile (/= 1) . iterate (y utilFun2)
+-- fun2' = sum . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
+
+-- the point is to use two pattern
+-- 片方を切り捨てて同じ処理をするだけだったらfilter で済むんだけど、
+-- そうじゃない
+
