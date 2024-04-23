b045a3f5cbc57962e7f3d877456e28eddc75d41b
kotto5 <kotto555555@gmail.com>
2024-04-23 23:58:37 +0900

    refactor

--- week4/homework.hs
+++ week4/homework.hs
@@ -8,6 +8,11 @@ fun1 (x:xs)
 fun1' :: [Integer] -> Integer
 fun1' = foldl' (*) 1 . map (\x -> x - 2) . filter even 
 
+
+-- the point is to use two pattern
+-- 片方を切り捨てて同じ処理をするだけだったらfilter で済むんだけど、
+-- そうじゃない
+
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n
@@ -22,9 +27,7 @@ utilFun2 f n = if even n then n `div` 2 else f (3 * n + 1)
 
 fun2' :: Integer -> Integer
 fun2' = sum . takeWhile (/= 1) . iterate (y utilFun2)
--- fun2' = sum . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
 
--- the point is to use two pattern
--- 片方を切り捨てて同じ処理をするだけだったらfilter で済むんだけど、
--- そうじゃない
+fun2'' :: Integer -> Integer
+fun2'' = sum . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
 
