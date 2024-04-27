b874ec7dfb2ef0ebae636885b06cda62d4ca550e
kotto5 <kotto555555@gmail.com>
2024-04-27 17:24:27 +0900

    一旦 foldTree 作ったが、高さが逆だった。 うーん。作り方を再帰的に下からやればいけそう

--- week4/homework.hs
+++ week4/homework.hs
@@ -31,3 +31,39 @@ fun2' = sum . takeWhile (/= 1) . iterate (y utilFun2)
 fun2'' :: Integer -> Integer
 fun2'' = sum . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
 
+fun2''' :: Integer -> Integer
+fun2''' = sum . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else (3 * x + 1) `div` 2)
+
+
+fun2'''' = iterate (\x -> if even x then x `div` 2 else (3 * x + 1) `div` 2)
+
+fun2''''' = sum . filter even . takeWhile (>1) . iterate hailstone
+  where hailstone n = if even n then n `div` 2 else 3*n + 1
+
+tester :: (Integer -> Integer) -> (Integer -> Integer) -> Integer -> Bool
+tester f f' x = f x == f' x
+
+-- 二重にeven すればいいのか〜なるほど
+-- hailstone の値が奇数になら
+-- ないこともあることで悩んでたけど、そこで分離するのね〜
+-- どっちの方がいいかは別として、hailstone というメガネを持ってたらこういう実装もできるな
+-- どうやってfun2 通りの実装をするか考えてしまっていた
+-- なかなか難しい
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+-- [each i lst | i <- [1..length lst]]
+
+toBiTree :: [a] -> Integer -> Tree a
+toBiTree [] _ = Leaf
+toBiTree (x:xs) height = Node height (toBiTree head (height + 1)) x (toBiTree tail (height + 1))
+    where
+        half_len = length xs `div` 2
+        head = take half_len xs
+        tail = drop half_len xs
+        
+
+foldTree :: [a] -> Tree a
+foldTree xs = toBiTree xs 0
